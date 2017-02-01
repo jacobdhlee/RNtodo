531f6c99221f5af0808b45dfa18ca851774c69e0
Donghee Lee <jacobdhlee@gmail.com>
2017-02-01 12:48:01 -0500

    incomplete animation view for buttons

--- component/Lists.js
+++ component/Lists.js
@@ -13,51 +13,87 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 const { width, height } = Dimensions.get('window');
 
 class Lists extends Component {
-  
+  constructor(props) {
+    super(props)
+    this.flipCard = this.flipCard.bind(this);
+  }
+
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(0)
+    this.value = 0
+    this.animatedValue.addListener(({value}) => this.value = value)
+    this.frontInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['0deg', '180deg'],
+    })
+
+    this.backInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['180deg', '360deg'],
+    })
+  }
+
+  flipCard() {
+    if(this.value > 90) {
+      Animated.spring(this.animatedValue, {
+        toValue: 0,
+        friction: 8,
+        tension: 10
+      }).start()
+    } else {
+       Animated.spring(this.animatedValue, {
+          toValue: 180,
+          friction: 8,
+          tension: 10
+        }).start()
+    }
+  }
+
   render() {
     const { thingsTodo } = this.props
 
     const starColor = thingsTodo.important ? "#f2d518" : '#1acc5e';
     const completeColor = thingsTodo.complete ? "#1acc5e" : 'lightgrey';
 
-
-    const renderHiddenContent =  (
-        <View style={styles.container}>
-          <TouchableOpacity style={{flex: 1, alignItems: 'center'}} onPress={() => this.props.hidden(true)}>
-            <Text>X</Text>
-          </TouchableOpacity>
-
-          <View style={styles.body}>
-            <TouchableOpacity style={styles.edit} onPress={() =>  this.props.edit()}>
-              <Text style={styles.buttonText}>Edit</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.delete} onPress={() => this.props.delete()}>
-              <Text style={styles.buttonText}>Delete</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      )
-    
-    const renderContent = (
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.starButton} onPress={() => this.props.important()}>
-          <Icon name="star" size={20} color={starColor}/>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.body} onLongPress={() => this.props.hidden(false)}>
-          <Text style={[styles.text, thingsTodo.complete && styles.completeText]}>{thingsTodo.todo}</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.completeButton} onPress={() => this.props.complete()}>
-          <Icon name="check" size={20} color={completeColor}/>
-        </TouchableOpacity>
-      </View>
-    )
+     const frontAmimateStyle = {
+      transform: [
+        {rotateY: this.frontInterpolate}
+      ]
+    }
+    const backAmimateStyle = {
+      transform: [
+        {rotateY: this.backInterpolate}
+      ]
+    }
 
     return (
-        <View>
-            { thingsTodo.hidden ? renderContent : renderHiddenContent }                     
+        <View style={styles.container}>
+          <Animated.View style={[frontAmimateStyle, styles.frontView]}>
+            <TouchableOpacity style={styles.starButton} onPress={() => this.props.important()}>
+              <Icon name="star" size={20} color={starColor}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.body} onPress={this.flipCard}>
+              <Text style={[styles.text, thingsTodo.complete && styles.completeText]}>{thingsTodo.todo}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.completeButton} onPress={() => this.props.complete()}>
+              <Icon name="check" size={20} color={completeColor}/>
+            </TouchableOpacity>
+          </Animated.View>
+
+          <Animated.View style={[backAmimateStyle, styles.frontView, styles.backView]}>
+            <TouchableOpacity style={styles.back} onPress={this.flipCard}>
+              <View />
+            </TouchableOpacity>
+              <TouchableOpacity style={styles.edit} onPress={() =>  this.props.edit()}>
+                <Text style={styles.buttonText}>Edit</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.delete} onPress={() => this.props.delete()}>
+                <Text style={styles.buttonText}>Delete</Text>
+              </TouchableOpacity>
+          </Animated.View>
         </View>           
     )
   }
@@ -66,19 +102,25 @@ class Lists extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
     height: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
+
   text: {
     fontSize: 20,
     fontWeight: '500'
   },
-  body: {
-    flex: 8,
+
+  frontView: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    backfaceVisibility: 'hidden',
+  },
+  
+  backView: {
+    position: 'absolute',
+    flexDirection: 'row'
   },
 
   starButton: {
@@ -87,36 +129,50 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     margin : 10
   },
+
+  body: {
+    flex: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  
   completeButton:{
     flex: 1,
     justifyContent: 'center',
     margin: 10,
     alignItems: 'center',
   },
+
   completeText: {
     color: 'darkgrey',
     textDecorationLine: 'line-through'
   },
+
   edit: {
-    width: width * 0.2,
+    width: width * 0.3,
     backgroundColor: 'lightgrey',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 5,
-    height: 40
-
+    margin: 5,
+    
   },
   delete: {
-    width: width * 0.2,
+    width: width * 0.3,
     backgroundColor: 'red',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 5,
-    height: 40
+    margin: 5
   },
   buttonText:{
     fontSize: 15,
     fontWeight: '500',
+  },
+
+  back: {
+    width: width * 0.2,
   }
   
 })
